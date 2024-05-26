969fd77f6ddf2b680a808b45710d1456f8f25e5c
Ivan Ovcharov <xxovcharovxx@gmail.com>
2024-05-26 13:55:50 +0300

    feat: added filtering by category, changed RecyclePoint(DTO) model

--- app/src/main/java/com/example/remap/ui/screens/map/MapViewModel.kt
+++ app/src/main/java/com/example/remap/ui/screens/map/MapViewModel.kt
@@ -10,7 +10,6 @@ import com.example.remap.domain.repository.RecyclePointRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -21,11 +20,23 @@ class MapViewModel @Inject constructor(
 
     val categoryType = MutableStateFlow(mutableListOf<String>())
 
+    val filterCategories = mutableListOf<String>()
+
     private val _recyclePoints = mutableStateListOf<RecyclePoint>()
-    val recyclePoints: List<RecyclePoint> = _recyclePoints
+    var recyclePoints: List<RecyclePoint> = _recyclePoints
 
     init {
         initRecyclePoint()
+    }
+
+    fun updateRecyclePoints() {
+        val categoryType = if(filterCategories.isEmpty()) null else filterCategories
+        viewModelScope.launch {
+            val response = repository.getRecyclePoints(categoryType = categoryType)
+            val handleResponse = response.body()?.map { it.toRecyclePoint() } ?: emptyList()
+            _recyclePoints.clear()
+            _recyclePoints.addAll(handleResponse)
+        }
     }
 
     fun addRecyclePoint(
